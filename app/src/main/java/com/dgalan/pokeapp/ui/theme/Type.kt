954cc1c3a2149a800f3a83120911ad6b49827d9a
Daniel Galan <danielgg944@gmail.com>
2023-09-16 13:36:00 +0200

    [Feat] Added app typography

--- app/src/main/java/com/dgalan/pokeapp/ui/theme/Type.kt
+++ app/src/main/java/com/dgalan/pokeapp/ui/theme/Type.kt
@@ -2,33 +2,84 @@ package com.dgalan.pokeapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.dgalan.pokeapp.R
+
+val Barlow = FontFamily(
+    Font(R.font.barlow_thin, FontWeight.Thin),
+    Font(R.font.barlow_light, FontWeight.Light),
+    Font(R.font.barlow_regular, FontWeight.Normal),
+    Font(R.font.barlow_medium, FontWeight.Medium),
+    Font(R.font.barlow_bold, FontWeight.Bold),
+    Font(R.font.barlow_black, FontWeight.Black),
+    Font(R.font.barlow_semibold, FontWeight.SemiBold),
+    Font(R.font.barlow_extralight, FontWeight.ExtraLight),
+    Font(R.font.barlow_extrabold, FontWeight.ExtraBold),
+)
 
 // Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+val AppTypography = Typography(
+    headlineLarge = TextStyle(
+        fontFamily = Barlow,
+        fontWeight = FontWeight.Light,
+        fontSize = 64.sp,
+        letterSpacing = (-0.5).sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = Barlow,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
+        fontSize = 36.sp,
+        letterSpacing = 0.25.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = Barlow,
+        fontWeight = FontWeight.Medium,
+        fontSize = 21.sp,
+        letterSpacing = 0.15.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = Barlow,
+        fontWeight = FontWeight.Normal,
+        fontSize = 17.sp,
+        letterSpacing = 0.15.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = Barlow,
+        fontWeight = FontWeight.Medium,
+        fontSize = 15.sp,
+        letterSpacing = 0.1.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = Barlow,
+        fontWeight = FontWeight.Normal,
+        fontSize = 17.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    ),
+    bodySmall = TextStyle(
+        fontFamily = Barlow,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 15.sp,
+        letterSpacing = 0.25.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = Barlow,
+        fontWeight = FontWeight.Medium,
+        fontSize = 15.sp,
+        letterSpacing = 1.25.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = Barlow,
+        fontWeight = FontWeight.Normal,
+        fontSize = 13.sp,
+        letterSpacing = 0.4.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
+        fontFamily = Barlow,
+        fontWeight = FontWeight.Normal,
         fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+        letterSpacing = 1.5.sp
+    ),
 )
