cf15b65ac1ea13a2610824885d5d2c77b7c9b636
danineon <67978045+danineon@users.noreply.github.com>
2023-09-19 11:30:44 +0200

    Feature/Feature-Login Screen (#4)
    * [Feat] Added new font and delete the old
    
    * [Feat] Added string resources
    
    * [Feat] Implemented material extensions for icons
    
    * [Feat] Created Design System and DSTextField
    
    * [Feat] Added logo PokeApp
    
    * [Feat] Added new font in Type.kt
    
    * [Feat] Designed new screen (LoginScreen)

--- app/src/main/java/com/dgalan/pokeapp/ui/theme/Type.kt
+++ app/src/main/java/com/dgalan/pokeapp/ui/theme/Type.kt
@@ -8,78 +8,84 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.dgalan.pokeapp.R
 
-val Barlow = FontFamily(
-    Font(R.font.barlow_thin, FontWeight.Thin),
-    Font(R.font.barlow_light, FontWeight.Light),
-    Font(R.font.barlow_regular, FontWeight.Normal),
-    Font(R.font.barlow_medium, FontWeight.Medium),
-    Font(R.font.barlow_bold, FontWeight.Bold),
-    Font(R.font.barlow_black, FontWeight.Black),
-    Font(R.font.barlow_semibold, FontWeight.SemiBold),
-    Font(R.font.barlow_extralight, FontWeight.ExtraLight),
-    Font(R.font.barlow_extrabold, FontWeight.ExtraBold),
+val Montserrat = FontFamily(
+    Font(R.font.montserrat_thin, FontWeight.Thin),
+    Font(R.font.montserrat_light, FontWeight.Light),
+    Font(R.font.montserrat_regular, FontWeight.Normal),
+    Font(R.font.montserrat_medium, FontWeight.Medium),
+    Font(R.font.montserrat_bold, FontWeight.Bold),
+    Font(R.font.montserrat_black, FontWeight.Black),
+    Font(R.font.montserrat_semibold, FontWeight.SemiBold),
+    Font(R.font.montserrat_extralight, FontWeight.ExtraLight),
+    Font(R.font.montserrat_extrabold, FontWeight.ExtraBold),
 )
 
 // Set of Material typography styles to start with
 val AppTypography = Typography(
     headlineLarge = TextStyle(
-        fontFamily = Barlow,
+        fontFamily = Montserrat,
         fontWeight = FontWeight.Light,
-        fontSize = 64.sp,
+        fontSize = 61.sp,
         letterSpacing = (-0.5).sp
     ),
     headlineMedium = TextStyle(
-        fontFamily = Barlow,
+        fontFamily = Montserrat,
         fontWeight = FontWeight.Normal,
-        fontSize = 36.sp,
+        fontSize = 35.sp,
         letterSpacing = 0.25.sp
     ),
     headlineSmall = TextStyle(
-        fontFamily = Barlow,
+        fontFamily = Montserrat,
         fontWeight = FontWeight.Medium,
-        fontSize = 21.sp,
-        letterSpacing = 0.15.sp
+        fontSize = 20.sp,
+        letterSpacing = 0.15.sp,
     ),
     titleMedium = TextStyle(
-        fontFamily = Barlow,
+        fontFamily = Montserrat,
         fontWeight = FontWeight.Normal,
-        fontSize = 17.sp,
+        fontSize = 16.sp,
         letterSpacing = 0.15.sp
     ),
     titleSmall = TextStyle(
-        fontFamily = Barlow,
+        fontFamily = Montserrat,
         fontWeight = FontWeight.Medium,
-        fontSize = 15.sp,
+        fontSize = 14.sp,
         letterSpacing = 0.1.sp
     ),
-    bodyMedium = TextStyle(
-        fontFamily = Barlow,
+    bodyLarge = TextStyle(
+        fontFamily = Montserrat,
         fontWeight = FontWeight.Normal,
-        fontSize = 17.sp,
+        fontSize = 16.sp,
+        letterSpacing = 0.5.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
         letterSpacing = 0.5.sp
     ),
     bodySmall = TextStyle(
-        fontFamily = Barlow,
+        fontFamily = Montserrat,
         fontWeight = FontWeight.Normal,
-        fontSize = 15.sp,
-        letterSpacing = 0.25.sp
+        fontSize = 12.sp,
+        letterSpacing = 0.sp
     ),
     labelLarge = TextStyle(
-        fontFamily = Barlow,
+        fontFamily = Montserrat,
         fontWeight = FontWeight.Medium,
-        fontSize = 15.sp,
+        fontSize = 14.sp,
         letterSpacing = 1.25.sp
     ),
     labelMedium = TextStyle(
-        fontFamily = Barlow,
+        fontFamily = Montserrat,
         fontWeight = FontWeight.Normal,
-        fontSize = 13.sp,
+        fontSize = 12.sp,
         letterSpacing = 0.4.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = Barlow,
+        fontFamily = Montserrat,
         fontWeight = FontWeight.Normal,
-        fontSize = 11.sp,
+        fontSize = 10.sp,
         letterSpacing = 1.5.sp
     ),
 )
