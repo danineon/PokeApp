0ae9c6cf1b77a228eba367d886e3df9d1f4fe56b
Daniel Galan <danielgg944@gmail.com>
2023-10-01 10:18:16 +0200

    [Feat] Added one tab button

--- app/src/main/java/com/dgalan/pokeapp/ui/designsystem/DSButton.kt
+++ app/src/main/java/com/dgalan/pokeapp/ui/designsystem/DSButton.kt
@@ -1,5 +1,6 @@
 package com.dgalan.pokeapp.ui.designsystem
 
+import android.os.SystemClock
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -7,6 +8,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -17,8 +22,14 @@ fun DSButton(
     onClick: () -> Unit,
     text: String
 ) {
+    var lastClickTime by remember { mutableLongStateOf(0L) }
     Button(
-        onClick = onClick,
+        onClick = {
+            if (SystemClock.elapsedRealtime() - lastClickTime > 1000) {
+                lastClickTime = SystemClock.elapsedRealtime()
+                onClick()
+            }
+        },
         modifier = Modifier
             .fillMaxWidth()
             .padding(horizontal = 72.dp),
