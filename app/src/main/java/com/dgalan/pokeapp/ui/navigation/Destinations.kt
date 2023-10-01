2010ed8872364c08dd5df81b909bb8d97b9317ec
Daniel Galan <danielgg944@gmail.com>
2023-10-01 10:14:58 +0200

    [Feat] Renamed Destinations to Screens

--- app/src/main/java/com/dgalan/pokeapp/ui/navigation/Destinations.kt
+++
@@ -1,6 +0,0 @@
-package com.dgalan.pokeapp.ui.navigation
-
-sealed class Destinations(val route: String) {
-    data object LoginScreen : Destinations("login_screen")
-    data object RegisterScreen : Destinations("register_screen")
-}
