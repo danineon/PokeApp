2e1a228c490ed5ce05a1eef7f25470c8b3a53cf8
Daniel Galan <danielgg944@gmail.com>
2023-10-01 10:14:39 +0200

    [Feat] Added parameter errorMessage in Resource

--- app/src/main/java/com/dgalan/pokeapp/utils/state/Resource.kt
+++ app/src/main/java/com/dgalan/pokeapp/utils/state/Resource.kt
@@ -2,7 +2,7 @@ package com.dgalan.pokeapp.utils.state
 
 sealed class Resource<out T> {
     data class Success<out T>(val data: T) : Resource<T>()
-    data class Error(val exception: Exception) : Resource<Nothing>()
+    data class Error(val exception: Exception, val errorMessage: String? = null) : Resource<Nothing>()
     data object Loading : Resource<Nothing>()
     data object Idle : Resource<Nothing>()
 }
