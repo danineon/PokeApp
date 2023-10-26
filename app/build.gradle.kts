337459535ba1fd050d4aee7f05c020f4e9258625
Daniel Galan <danielgg944@gmail.com>
2023-10-26 12:38:27 +0200

    [Feat] Updated dependencies of version catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,12 +56,12 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 
     buildFeatures {
@@ -99,6 +99,7 @@ dependencies {
     implementation(libs.lottie.compose)
     ksp(libs.hilt.android.compiler)
     testImplementation(libs.junit)
+    testImplementation(libs.mockk)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
     androidTestImplementation(platform(libs.compose.bom))
