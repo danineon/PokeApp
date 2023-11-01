c35d08e96ed200dc3b0857b6af4a09b96204f136
Daniel Galan <danielgg944@gmail.com>
2023-11-01 10:04:18 +0100

    [Feat] Added PokemonScreen in Navigation

--- app/src/main/java/com/dgalan/pokeapp/ui/navigation/Screens.kt
+++ app/src/main/java/com/dgalan/pokeapp/ui/navigation/Screens.kt
@@ -3,4 +3,5 @@ package com.dgalan.pokeapp.ui.navigation
 sealed class Screens(val route: String) {
     data object LoginScreen : Screens("login_screen")
     data object RegisterScreen : Screens("register_screen")
+    data object PokemonScreen : Screens("pokemon_screen")
 }
