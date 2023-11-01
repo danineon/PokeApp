87ff60d7e2464f5d4611bfe3a7c0af46c388c448
Daniel Galan <danielgg944@gmail.com>
2023-11-01 10:00:54 +0100

    [Feat] Added provides of Hilt

--- app/src/main/java/com/dgalan/pokeapp/authetication/data/di/FirebaseAuthModule.kt
+++ app/src/main/java/com/dgalan/pokeapp/authetication/data/di/FirebaseAuthModule.kt
@@ -1,21 +1,24 @@
-package com.dgalan.pokeapp.utils.di
+package com.dgalan.pokeapp.authetication.data.di
 
-import com.dgalan.pokeapp.firebasedata.FirebaseAuthRepositoryContract
-import com.dgalan.pokeapp.firebasedata.repository.FirebaseAuthRepository
+import com.dgalan.pokeapp.authetication.data.repository.FirebaseAuthRepository
+import com.dgalan.pokeapp.authetication.data.repository.FirebaseAuthRepositoryContract
 import com.google.firebase.auth.FirebaseAuth
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 class FirebaseAuthModule {
 
+    @Singleton
     @Provides
     fun provideFirebaseAuth(): FirebaseAuth =
         FirebaseAuth.getInstance()
 
+    @Singleton
     @Provides
     fun provideFirebaseAuthRepository(firebaseAuthRepository: FirebaseAuthRepository): FirebaseAuthRepositoryContract =
         firebaseAuthRepository
--- app/src/main/java/com/dgalan/pokeapp/utils/di/CoroutineDispatcherModule.kt
+++ app/src/main/java/com/dgalan/pokeapp/utils/di/CoroutineDispatcherModule.kt
@@ -7,24 +7,29 @@ import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import javax.inject.Qualifier
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object CoroutineDispatcherModule {
 
     @IoDispatcher
+    @Singleton
     @Provides
     fun providesIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
 
     @DefaultDispatcher
+    @Singleton
     @Provides
     fun providesDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
 
     @MainDispatcher
+    @Singleton
     @Provides
     fun providesMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
 
     @UnconfinedDispatcher
+    @Singleton
     @Provides
     fun providesUnconfinedDispatcher(): CoroutineDispatcher = Dispatchers.Unconfined
 
